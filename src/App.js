f20934b3c8ec313a4891a7430f8e0d25cf417eb7
rajeev1503 <rk.rajeevkr1503@gmail.com>
2023-06-09 01:56:48 +0530

    major changes 6

--- src/App.js
+++ src/App.js
@@ -21,7 +21,29 @@ export default function Home() {
       config: {
         iceServers: [
           { url: "stun:stun.l.google.com:19302" },
-          { url: "turn:homeo@turn.bistri.com:80", credential: "homeo" },
+          {
+            urls: "stun:a.relay.metered.ca:80",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:80",
+            username: "80f2af5598002ac9a80cc167",
+            credential: "jo5Km/7SHDPnJ/0S",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:80?transport=tcp",
+            username: "80f2af5598002ac9a80cc167",
+            credential: "jo5Km/7SHDPnJ/0S",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:443",
+            username: "80f2af5598002ac9a80cc167",
+            credential: "jo5Km/7SHDPnJ/0S",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:443?transport=tcp",
+            username: "80f2af5598002ac9a80cc167",
+            credential: "jo5Km/7SHDPnJ/0S",
+          },
         ],
       },
     });
